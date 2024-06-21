69a7105e2a56ea3830888d1d54daa3d5fb378ffb
Siphamandla Zolakhe <zwelakhespha@gmail.com>
2024-06-21 12:44:46 +0200

    Added Dependency Injection and Controllers

---
+++ activities.api/Controllers/ActivitiesController.cs
@@ -0,0 +1,55 @@
+using activities.api.CQRS.Commands;
+using activities.api.CQRS.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace activities.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActivitiesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ActivitiesController(IMediator mediator)
+        {
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetActivities()
+        {
+            var activities = await _mediator.Send(new GetActivitiesQuery());
+            return Ok(activities);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetActivity(int id)
+        {
+            var activity = await _mediator.Send(new GetActivityByIdQuery { Id = id });
+            return Ok(activity);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity([FromBody] CreateActivityCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateActivity(int id, [FromBody] UpdateActivityCommand command)
+        {
+            command.Id = id;
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteActivity(int id)
+        {
+            var result = await _mediator.Send(new DeleteActivityCommand { Id = id });
+            return Ok(result);
+        }
+    }
+}
--- activities.api/Program.cs
+++ activities.api/Program.cs
@@ -1,10 +1,22 @@
+using System.Reflection;
+using activities.api.Data;
+using activities.api.Repositories;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(swagg => 
+            {
+                swagg.SwaggerDoc("v1", new OpenApiInfo { Title = "TODO API", Version = "v1" });
+            });
+
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining(typeof(Program)));
+builder.Services.AddDbContext<ActivitiesDbContext>();
+builder.Services.AddScoped<IActivityRepository, ActivityRepository>();
 
 var app = builder.Build();
 
